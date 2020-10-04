140a1f84079fe9d3c96328f6f1190a993c0cf91d
abdelrahman403 <abdelrhmanhassan882@gmail.com>
2020-10-05 01:49:36 +0200

    Improve logging & Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=abdelrahman16/udacityapp
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag udacity-dev:latest $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
